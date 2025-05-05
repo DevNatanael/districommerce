c43266c57891a64687f614bb573d619aa5d4790c
natan <natan@example.com>
2025-05-05 16:31:33 -0300

    separando serviço de pedidos

--- src/main/java/com/districommerce/DistriCommerce/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.districommerce.DistriCommerce.repository;
-
-import com.districommerce.DistriCommerce.entity.Pedido;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface PedidoRepository extends MongoRepository<Pedido, String> {
-    List<Pedido> findByUsuarioId(Long usuarioId);
-}
